98ec729d44745b62d988d29e3c4ec0236ab4a796
CHIGY <gdavid.ptorrez@gmail.com>
2018-03-30 22:55:50 -0400

    Cambiando content por defecto

--- views-react/src/components/Item/Item.js
+++ views-react/src/components/Item/Item.js
@@ -86,7 +86,7 @@ Item.defaultProps = {
   type: 'Info',
   line: '0.0',
   nameFile: 'app.js',
-  content: 'Now that we know who you are, I know who I am. I\'m not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain\'s going to be? He\'s the exact opposite of the hero. And most times they\'re friends, like you and me! I should\'ve known way back when... You know why, David? Because of the kids. They called me Mr Glass.',
+  content: 'Esto es un ejemplo.',
   routeFile: '/home/Codersign'
 };
 
