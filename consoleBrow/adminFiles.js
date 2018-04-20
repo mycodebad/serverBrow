d9a9d78855379de88308e2b53781a09f861f1fcf
adrianZelada <zelada_torrez@hotmail.com>
2018-04-19 23:16:15 -0400

    logs

--- consoleBrow/adminFiles.js
+++ consoleBrow/adminFiles.js
@@ -1,31 +1,72 @@
 let fs = require('fs');
-
+let path = require('path');
 module.exports = function(){
+    let lib  = `${path.join(path.dirname(fs.realpathSync(__filename)))}/`;
+    console.log("lib");
+    console.log(lib);    
     let read = (path)=>{
         return new Promise((resolve, reject)=>{
-            fs.readFile(path,( err, data)=>{
+            fs.readFile(lib+path,{
+                flag:'a+',
+                encoding:'utf8'
+            },( err, data)=>{
                 if(err){
+                    console.warn('eeeee')
                     reject(err);
                 }else{
-                    resolve(JSON.parse(data));
+                    console.warn('daaa')
+                    console.warn(data=='')
+                    
+                    resolve(data);
                 }                
             });
         })        
     }
 
     let update = (path,data)=>{
+        // return new Promise((resolve, reject)=>{
+        //     fs.readFile(lib+path,'utf8',( err, result)=>{
+        //         if(err){
+        //             console.warn("errrrr");                
+        //             reject(err);
+        //         }else{
+        //             console.warn("result");
+        //             console.warn(result=='');                    
+        //             let table = result=='' ? [] : JSON.parse(result);
+        //             table.unshift(data)
+        //             console.warn(table)
+        //             fs.writeFile(path,JSON.stringify(table),'utf8',(error,data)=>{
+        //                 if(error){
+        //                     reject(error);
+        //                 }else{
+        //                     resolve(table);
+        //                 }
+        //             });                    
+        //         }                
+        //     });
+        // })
+        
         return new Promise((resolve, reject)=>{
-            fs.readFile(path,( err, result)=>{
+            fs.readFile(lib+path,'utf8',( err, result)=>{
                 if(err){
+                    console.warn("errrrr");                
                     reject(err);
                 }else{
-                    let table = JSON.parse(result);
+                    console.warn("result");
+                    console.warn(result);                    
+                    let table = result=='' ? [] : JSON.parse(result);
                     table.unshift(data)
-                    fs.writeFile(path,JSON.stringify(table),'utf8');
-                    resolve(table);
+                    console.warn(table)
+                    fs.writeFile(path,JSON.stringify(table),'utf8',(error,data)=>{
+                        if(error){
+                            reject(error);
+                        }else{
+                            resolve(table);
+                        }
+                    });                    
                 }                
             });
-        })        
+        })
     }
 
     return {
--- consoleBrow/socket.js
+++ consoleBrow/socket.js
@@ -2,10 +2,27 @@ let socket = require('socket.io');
 let file = require('./adminFiles')();
 
 module.exports = function (eventEmitter){    
-    let io = socket(8888);    
+    let io = socket(8888);
+    file.read('log.json').then((data)=>{
+        console.warn("data");
+        console.warn(data);        
+    }).catch((e)=>{
+        console.warn("error");
+        console.warn(e);  
+    })    
+
     eventEmitter.on('log',(data)=>{            
-        data.id = Date.now();
-        io.sockets.emit('log', data);        
+        // data.id = Date.now();
+        // io.sockets.emit('log', data);     
+        file.update('log.json',data).then((d)=>{
+            console.warn("data");
+            console.warn(d);  
+            data.id = Date.now();
+            io.sockets.emit('log', data);          
+        }).catch((e)=>{
+            console.warn("error");
+            console.warn(e);  
+        })      
     });
 
     eventEmitter.on('middleware',(data)=>{        
