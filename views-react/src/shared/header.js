eb469b8d19596a5fef7aeadef99ea469424b849c
Guillermo David Paredes Torrez <gdavid.ptorrez@gmail.com>
2018-06-12 20:39:45 -0400

    Cambiand link de logo

--- views-react/src/shared/header.js
+++ views-react/src/shared/header.js
@@ -4,8 +4,8 @@
  * @link    url goes here
  */
 
-import React, { Component } from 'react';
-import './header.scss';
+import React, { Component } from "react";
+import "./header.scss";
 
 class Header extends Component {
   render() {
@@ -17,27 +17,43 @@ class Header extends Component {
               <i className="icon-close" />
             </button>
             <form id="searchForm" role="search">
-              <input type="search" placeholder="What are you looking for..." className="form-control" />
+              <input
+                type="search"
+                placeholder="What are you looking for..."
+                className="form-control"
+              />
             </form>
           </div>
           <div className="container-fluid">
             <div className="navbar-holder d-flex align-items-center justify-content-between">
               <div className="navbar-header">
-                <a href="index.html" className="navbar-brand">
-                <div className="brand-text brand-big">
-                  <span>Server </span><strong>Browser</strong>
-                </div>
-                <div className="brand-text brand-small"><strong>Server Browser</strong></div></a>
-                  <a id="toggle-btn"  className="menu-btn active">
-                    <span /><span /><span />
-                  </a>
+                <a className="navbar-brand">
+                  <div className="brand-text brand-big">
+                    <span>Server </span>
+                    <strong>Browser</strong>
+                  </div>
+                  <div className="brand-text brand-small">
+                    <strong>Server Browser</strong>
+                  </div>
+                </a>
+                <a id="toggle-btn" className="menu-btn active">
+                  <span />
+                  <span />
+                  <span />
+                </a>
               </div>
               <ul className="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
                 <li className="nav-item d-flex align-items-center">
-                  <a id="search" ><i className="fa fa-search" /></a>
+                  <a id="search">
+                    <i className="fa fa-search" />
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a href="https://github.com/mycodebad" className="nav-link logout">Repositorio
+                  <a
+                    href="https://github.com/mycodebad"
+                    className="nav-link logout"
+                  >
+                    Repositorio
                     <i className="fa fa-github" />
                   </a>
                 </li>
